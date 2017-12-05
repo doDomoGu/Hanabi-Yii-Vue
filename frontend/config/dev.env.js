8fc3d0b7d773a8e8232ed70fc74e53c1f2f94e6c
doDomoGu <kaka_71936410@qq.com>
2017-12-05 12:23:00 +0800

    开发环境配置

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,6 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  AXIOS_BASEURL: '"http://api.localhanabi-yii.com/v1"',
+  //AXIOS_BASEURL: '"http://api.localhanabi-yii.com/v1"',
+  AXIOS_BASEURL: '"http://192.168.0.100:8889/v1"',
 
 })
