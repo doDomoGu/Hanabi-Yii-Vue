a5a772328938f6cc431ce0b10fd1f6b5e9c93a63
doDomoGu <kaka_71936410@qq.com>
2017-12-13 11:16:54 +0800

    前端 手牌样式

--- frontend/src/views/game/Index.vue
+++ frontend/src/views/game/Index.vue
@@ -1,39 +1,26 @@
 <template>
     <div id="game">
-        <section>
-            <div class="player1 player-block">
-                房主：
-                <div v-if="master_user.id>0" class="player-info">
+        <section :class="'player-block' + (master_user.is_you?' is_you':'')">
+            <div class="player-name">房主： {{'('+master_user.id+')'+master_user.name}}</div>
 
-                    <div>{{'('+master_user.id+(master_user.is_you?'*':'')+')'+master_user.name}}</div>
-
-                    <div class="hand-card">
-
-                        <li v-for="card in master_user.cards" >
-                            <span :class="colors[card.color]+'-color'">
-                                {{card.color+' - '+card.num}}
-                            </span>
-                        </li>
-                    </div>
-                </div>
+            <div class="hand-card">
+                <li v-for="card in master_user.cards" :class="colors[card.color]+'-color'">
+                    <span>{{numbers[card.num]}}</span>
+                </li>
             </div>
         </section>
+        <section class="table-block">
+            <div class="library-block"></div>
+            <div class="box-block"></div>
+            <div class="discard-block"></div>
+        </section>
+        <section :class="'player-block' + (guest_user.is_you?' is_you':'')">
+            <div class="player-name">玩家：{{'('+guest_user.id+')'+guest_user.name}}</div>
 
-        <section>
-            <div class="player2 player-block">
-                玩家：
-                <div v-if="guest_user.id>0" class="player-info">
-
-                    <div>{{'('+guest_user.id+(guest_user.is_you?'*':'')+')'+guest_user.name}}</div>
-
-                    <div class="hand-card">
-                        <li v-for="card in guest_user.cards" >
-                            <span :class="colors[card.color]+'-color'">
-                                {{card.color+' - '+card.num}}
-                            </span>
-                        </li>
-                    </div>
-                </div>
+            <div class="hand-card">
+                <li v-for="card in guest_user.cards" :class="colors[card.color]+'-color'">
+                    <span>{{numbers[card.num]}}</span>
+                </li>
             </div>
         </section>
         <mt-button v-if="master_user.is_you>0" @click.native="endGame" size="large" class="game-end-btn" type="danger">结束游戏</mt-button>
