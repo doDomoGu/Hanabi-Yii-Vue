69706b7f142453c0cd88c534f3a771d33d365a71
doDomoGu <kaka_71936410@qq.com>
2017-12-29 14:51:33 +0800

    前端改写 canvas

--- api/config/web.php
+++ api/config/web.php
@@ -78,7 +78,7 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1','192.168.0.100','220.248.123.222'],
     ];
 
     $config['bootstrap'][] = 'gii';
--- frontend/src/assets/js/room/index.js
+++ frontend/src/assets/js/room/index.js
@@ -1,69 +1,27 @@
-import { MessageBox } from 'mint-ui'
 export default {
-  name: 'room',
+  name: 'room2',
   data () {
     return {
     }
   },
   mounted: function(){
-
+    let canvas = document.querySelector('canvas'),
+      ctx = canvas.getContext('2d')
+    /*console.log(window);
+    console.log(canvas);*/
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = "#cececa";
+    ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+    /*ctx.lineWidth = .3;
+    ctx.strokeStyle = (new Color(150)).style;*/
   },
   created: function(){
-
-    this.$store.dispatch('my_room/IsInRoom').then(()=>{
-
-      this.$store.dispatch('common/SetTitle2','房间'+this.$store.getters['my_room/room_id']);
-      this.getRoomInfo();
-
-      this.intervalid1 = setInterval(()=>{
-        this.getRoomInfo();
-        this.$store.dispatch('my_game/IsInGame').then(()=>{
-          if(this.$store.getters['my_game/is_playing']){
-            this.$router.push('/game');
-          }
-        });
-      },500);
-
-    });
-  },
-  beforeDestroy () {
-    clearInterval(this.intervalid1)
   },
   computed : {
-    is_host:function() {
-      return this.$store.getters['my_room/is_host'];
-    },
-    host_player:function(){
-      return this.$store.getters['my_room/host_player'];
-    },
-    guest_player:function(){
-      return this.$store.getters['my_room/guest_player'];
-    }
+
   },
   methods: {
-    exit(){
-      MessageBox.confirm('确定要退出房间?').then(action => {
-        if(action==='confirm'){
-          this.$store.dispatch('my_room/Exit').then(()=>{
-            this.$router.push('/');
-          });
-        }else{
-          return false;
-        }
-      });
-    },
-    getRoomInfo(){
-      this.$store.dispatch('my_room/GetRoomInfo');
-    },
-    doReady(){
-      this.$store.dispatch('my_room/DoReady');
-    },
-    startGame(){
-      this.$store.dispatch('my_game/Start').then((res)=>{
-        if(res.success){
-          this.$router.push('/game');
-        }
-      })
-    }
+
   }
 }
